ed4d6bf1437ccdd6923e2f90eb63af483f980153
Michal G <33378775+Galajda@users.noreply.github.com>
2018-02-10 09:34:12 -0600

    Second attempt
    Trying to integrate GitHub into Android Studio

---
+++ app/src/main/java/com/cogimag/michalg/nachocanvas/CanvasView.java
@@ -0,0 +1,107 @@
+package com.cogimag.michalg.nachocanvas;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.graphics.Path;
+import android.support.constraint.solver.widgets.Rectangle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by MichalG HP Envy on 2/8/2018.
+ */
+
+public class CanvasView extends View {
+    private float xStart, yStart;
+    static Paint paint;
+    Path path;
+    Boolean clear;
+    public CanvasView(Context ctxt, AttributeSet attrs) {
+        super(ctxt, attrs);
+        paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.BLACK);
+        path = new Path();
+        clear = true;
+    }
+
+    @Override
+    protected void onDraw(Canvas cnv) {
+        if (clear) {
+            path.reset();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            cnv.drawPaint(paint);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(5);
+            paint.setColor(Color.BLACK);
+            clear = false;
+        }
+        else  {
+            super.onDraw(cnv);
+            cnv.drawPath(path,paint);
+        }
+    }
+
+    protected boolean handleTouch(MotionEvent event) {
+//        Log.i("canvas view", "handling touch event");
+        float xCurrent = event.getX();
+        float yCurrent = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(xCurrent,yCurrent);
+                xStart = xCurrent;
+                yStart = yCurrent;
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+//                Log.i("handle touch", "action move");
+                path.lineTo(xCurrent,yCurrent);
+                invalidate();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                path.lineTo(xCurrent,yCurrent);
+//                invalidate();
+                break;
+        }
+        return true;
+    }
+    protected void clearCanvas() {
+        clear = true;
+        invalidate();
+    }
+    protected static void changeColor(String color) {
+        paint.setStrokeWidth(5);
+        switch (color) {
+            case "Red":
+                paint.setColor(Color.RED);
+                break;
+            case "Orange":
+                paint.setColor(Color.rgb(255,165, 0));
+                break;
+            case "Yellow":
+                paint.setColor(Color.YELLOW);
+                break;
+            case "Green":
+                paint.setColor(Color.GREEN);
+                break;
+            case "Blue":
+                paint.setColor(Color.BLUE);
+                break;
+            case "Black":
+                paint.setColor(Color.BLACK);
+                break;
+            case "Eraser":
+                paint.setColor(Color.WHITE);
+                paint.setStrokeWidth(15);
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/cogimag/michalg/nachocanvas/MainActivity.java
@@ -0,0 +1,59 @@
+package com.cogimag.michalg.nachocanvas;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    CanvasView canvasView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Spinner colorSpinner = (Spinner)findViewById(R.id.spnColorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_choices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(adapter);
+        colorSpinner.setOnItemSelectedListener(this);
+        canvasView = (CanvasView)findViewById(R.id.cnvLeinwand);
+
+        canvasView.setOnTouchListener(
+                new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+//                        Log.i("canvas view", "on touch listener");
+//                        return true;
+                        return canvasView.handleTouch(event);
+                    }
+                }
+        );
+    }
+    public void btnClearCanvas_Click(View view) {
+//        Log.i("main activity", "btn clear click");
+//        canvasView = (CanvasView)findViewById(R.id.cnvLeinwand);
+        canvasView.clearCanvas();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("spinner selected", "item " + parent.getItemAtPosition(position));
+        CanvasView.changeColor((String) parent.getItemAtPosition(position).toString());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
